8b61ed96489b60de8b5a5d938d485a9d1f6394bc
Eduardo Castillo <crypsoft1@gmail.com>
2022-03-25 23:37:08 -0400

    commit final

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["index.js", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {},
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
